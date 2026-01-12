05b1f2833f110a9c48f175a7520103a65ecbcb55
Max Kranker <maxkranker@protonmail.com>
2026-01-12 17:26:08 +0700

    Add test for SessionLoad middleware handler

--- cmd/web/middleware_test.go
+++ cmd/web/middleware_test.go
@@ -9,7 +9,20 @@ func TestNoSurf(t *testing.T) {
 	var myH myHandler
 	h := NoSurf(&myH)
 
-	// test if return of NoSurf is
+	// test if return is http.Handler
+	switch v := h.(type) {
+	case http.Handler:
+		// do nothing, test passed
+	default:
+		t.Errorf("type is not http.Handler, but is %T", v)
+	}
+}
+
+func TestSessionLoad(t *testing.T) {
+	var myH myHandler
+	h := SessionLoad(&myH)
+
+	// test if return is http.Handler
 	switch v := h.(type) {
 	case http.Handler:
 		// do nothing, test passed
