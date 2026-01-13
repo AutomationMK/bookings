652a0637934d57a5ad0bcfdfe083f81b10b8ed26
Max Kranker <maxkranker@protonmail.com>
2026-01-13 20:21:56 +0700

    Add initial ClientError function

--- internal/helpers/helpers.go
+++ internal/helpers/helpers.go
@@ -1,10 +1,18 @@
 package helpers
 
-import "github.com/AutomationMK/bookings/internal/config"
+import (
+	"net/http"
+
+	"github.com/AutomationMK/bookings/internal/config"
+)
 
 var app *config.AppConfig
 
 // NewHelpers sets up appConfig for helpers
 func NewHelpers(a *config.AppConfig) {
 	app = a
+}
+
+func ClientError(w http.ResponseWriter, status int) {
+
 }
