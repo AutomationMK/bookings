999d397e5451cbf4a534fd9d1f3070563c922727
Max Kranker <maxkranker@protonmail.com>
2026-01-13 19:57:29 +0700

    Add test for Errors Get method

--- internal/forms/errors_test.go
+++ internal/forms/errors_test.go
@@ -18,3 +18,20 @@ func TestErrors_Add(t *testing.T) {
 		t.Errorf("added more than 1 error field, added %d error fields instead", lenErr)
 	}
 }
+
+func TestErrors_Get(t *testing.T) {
+	var err = errors(map[string][]string{})
+	testField := "test"
+	testMsg := "This is a test"
+	err[testField] = append(err[testField], testMsg)
+
+	testGet := err.Get(testField)
+	if testGet != testMsg {
+		t.Errorf("has \"%s\" as returned message when it should be \"%s\"", testGet, testMsg)
+	}
+	testGet = err.Get("non-exist")
+	if testGet != "" {
+		t.Errorf("has %s when it should be empty for non-existent field", testGet)
+	}
+
+}
