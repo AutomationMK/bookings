f62f75bd8c631e4fec3ff32db6b9b84a27ba95fb
Max Kranker <maxkranker@protonmail.com>
2026-01-13 20:19:40 +0700

    Add description comment for NewHelpers()

--- internal/helpers/helpers.go
+++ internal/helpers/helpers.go
@@ -4,6 +4,7 @@ import "github.com/AutomationMK/bookings/internal/config"
 
 var app *config.AppConfig
 
+// NewHelpers sets up appConfig for helpers
 func NewHelpers(a *config.AppConfig) {
 	app = a
 }
