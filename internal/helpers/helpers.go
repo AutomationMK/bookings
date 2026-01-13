efd192dad6d86d42ed872a1f6d12c752faea4187
Max Kranker <maxkranker@protonmail.com>
2026-01-13 20:58:37 +0700

    Fix stack trace to show where the error happened

--- internal/helpers/helpers.go
+++ internal/helpers/helpers.go
@@ -22,6 +22,6 @@ func ClientError(w http.ResponseWriter, status int) {
 
 func ServerError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
-	app.ErrorLog.Println(trace)
+	app.ErrorLog.Output(2, trace)
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
