15e6d330c5265a1ac33160ef78f55ea1a5c07dc4
Max Kranker <maxkranker@protonmail.com>
2026-01-13 21:16:19 +0700

    Add infoLog and errorLog AppConfig vars to render tests

--- internal/render/setup_test.go
+++ internal/render/setup_test.go
@@ -2,6 +2,7 @@ package render
 
 import (
 	"encoding/gob"
+	"log"
 	"net/http"
 	"os"
 	"testing"
@@ -18,6 +19,12 @@ var testApp config.AppConfig
 func TestMain(m *testing.M) {
 	// change this to true when in production
 	testApp.InProduction = false
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	testApp.InfoLog = infoLog
+
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	testApp.ErrorLog = errorLog
 
 	// specify special structs that will be stored in the session
 	gob.Register(models.Reservation{})
