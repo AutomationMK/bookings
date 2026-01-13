fedfbaa3fb5e4a5f5c810471bb01d97f68929999
Max Kranker <maxkranker@protonmail.com>
2026-01-13 21:03:06 +0700

    Add errorLog usage

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -3,11 +3,11 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/AutomationMK/bookings/internal/config"
 	"github.com/AutomationMK/bookings/internal/forms"
+	"github.com/AutomationMK/bookings/internal/helpers"
 	"github.com/AutomationMK/bookings/internal/models"
 	"github.com/AutomationMK/bookings/internal/render"
 )
@@ -59,7 +59,7 @@ func (m *Repository) Reserve(w http.ResponseWriter, r *http.Request) {
 func (m *Repository) PostReserve(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
-		log.Println(err)
+		helpers.ServerError(w, err)
 		return
 	}
 
@@ -95,7 +95,7 @@ func (m *Repository) PostReserve(w http.ResponseWriter, r *http.Request) {
 func (m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) {
 	reservation, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
 	if !ok {
-		log.Println("cannot get item from session")
+		m.App.ErrorLog.Println("cannot get item from sessio")
 		m.App.Session.Put(r.Context(), "error", "Can't get reservation from session")
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
@@ -137,7 +137,8 @@ func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
 
 	out, err := json.MarshalIndent(resp, "", "     ")
 	if err != nil {
-		log.Println(err)
+		helpers.ServerError(w, err)
+		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
