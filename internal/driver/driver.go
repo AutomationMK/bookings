689437f3f375a97537d070518e7923a1907516c9
Max Kranker <maxkranker@protonmail.com>
2026-01-16 19:24:48 +0700

    Add initial database connection logic

---
+++ internal/driver/driver.go
@@ -0,0 +1,61 @@
+package driver
+
+import (
+	"database/sql"
+	"time"
+)
+
+// DB holds the database connection pool
+type DB struct {
+	SQL *sql.DB
+}
+
+var dbConn = &DB{}
+
+const maxOpenDbConn = 10
+const maxIdleDbConn = 5
+const maxDbLifetime = 5 * time.Minute
+
+// ConnectSQL creates dabase pool for postgres
+func ConnectSQL(dsn string) (*DB, error) {
+	d, err := NewDatabase(dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	d.SetMaxOpenConns(maxOpenDbConn)
+	d.SetConnMaxIdleTime(maxIdleDbConn)
+	d.SetConnMaxLifetime(maxDbLifetime)
+
+	dbConn.SQL = d
+
+	err = testDb(d)
+	if err != nil {
+		return nil, err
+	}
+
+	return dbConn, nil
+}
+
+// testDb tries to ping the database
+func testDb(d *sql.DB) error {
+	err := d.Ping()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// NewDatabase creates a new database for the application
+func NewDatabase(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pqx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
