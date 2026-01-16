969c226511969088f62081fe005d79a084b6ebb2
Max Kranker <maxkranker@protonmail.com>
2026-01-16 23:00:11 +0700

    Implement InsertReservation method and set up data

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/AutomationMK/bookings/internal/config"
 	"github.com/AutomationMK/bookings/internal/driver"
@@ -68,11 +70,33 @@ func (m *Repository) PostReserve(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// parse date data
+	ad := r.Form.Get("arrival_date")
+	dd := r.Form.Get("departure_date")
+	layout := "1/2/2006"
+	arrivalDate, err := time.Parse(layout, ad)
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+	departureDate, err := time.Parse(layout, dd)
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+
+	// convert room_id to integer
+	roomID, err := strconv.Atoi(r.Form.Get("room_id"))
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+
 	reservation := models.Reservation{
-		FirstName: r.Form.Get("first_name"),
-		LastName:  r.Form.Get("last_name"),
-		Email:     r.Form.Get("email"),
-		Phone:     r.Form.Get("phone"),
+		FirstName:     r.Form.Get("first_name"),
+		LastName:      r.Form.Get("last_name"),
+		Email:         r.Form.Get("email"),
+		Phone:         r.Form.Get("phone"),
+		ArrivalDate:   arrivalDate,
+		DepartureDate: departureDate,
+		RoomID:        roomID,
 	}
 
 	form := forms.New(r.PostForm)
@@ -90,6 +114,11 @@ func (m *Repository) PostReserve(w http.ResponseWriter, r *http.Request) {
 			Data: data,
 		})
 		return
+	}
+
+	err = m.DB.InsertReservation(reservation)
+	if err != nil {
+		helpers.ServerError(w, err)
 	}
 
 	m.App.Session.Put(r.Context(), "reservation", reservation)
