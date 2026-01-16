d4f05908925fce2f672233c7d0e61a34b1311377
Max Kranker <maxkranker@protonmail.com>
2026-01-16 21:18:13 +0700

    Add model for restrictions table

--- internal/models/models.go
+++ internal/models/models.go
@@ -29,3 +29,11 @@ type Rooms struct {
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
+
+// Restrictions holds restrictions data
+type Restrictions struct {
+	ID              int
+	RestrictionName string
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+}
