a4201c5b5dced3ff991cb306c3e7520dbd49bfbb
Max Kranker <maxkranker@protonmail.com>
2026-01-16 21:14:53 +0700

    Add model for users table

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,9 +1,23 @@
 package models
 
+import "time"
+
 // Reservation holds reservation data
 type Reservation struct {
 	FirstName string
 	LastName  string
 	Email     string
 	Phone     string
+}
+
+// Users holds user data
+type Users struct {
+	ID          int
+	FirstName   string
+	LastName    string
+	Email       string
+	Password    string
+	AccessLevel int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
 }
