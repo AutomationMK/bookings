062bbfafef0e40fddd98db0c3925a7672e5bdd80
Max Kranker <maxkranker@protonmail.com>
2026-01-17 13:10:59 +0700

    Change InsertRoomRestriction param to RoomRestriction model

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -5,5 +5,5 @@ import "github.com/AutomationMK/bookings/internal/models"
 type DatabaseRepo interface {
 	AllUsers() bool
 	InsertReservation(res models.Reservation) (int, error)
-	InsertRoomRestriction(res models.Reservation) error
+	InsertRoomRestriction(res models.RoomRestriction) error
 }
