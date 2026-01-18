d65d45b73f07264b28cbafb97ab6e25b92c5bd8e
Max Kranker <maxkranker@protonmail.com>
2026-01-18 23:36:15 +0700

    Change msg type to be a class object

--- static/js/notify.js
+++ static/js/notify.js
@@ -2,35 +2,41 @@ export default class Notice {
     msgBox;
     noticeEl;
     messageEl;
+    msgType;
 
-    constructor(msgBoxSel, id, style = "w-screen") {
+    constructor(msgBoxSel, type, style = "w-screen") {
         this.msgBox = document.querySelector(`${msgBoxSel}`);
+        this.msgType = type;
 
         this.msgBox.insertAdjacentHTML(
             "beforeend",
             `
-            <div id="${id}" class="${style} flex mx-auto h-fit justify-center ease-in-out duration-500 notify-close overflow-hidden">
+            <div id="${type}" class="${style} flex mx-auto h-fit justify-center ease-in-out duration-500 notify-close overflow-hidden">
                 <h2 class="container px-4 md:text-lg lg:text-xl font-bold text-center"></h2>
             </div>
             `,
         );
 
-        this.noticeEl = this.msgBox.querySelector(`#${id}`);
+        this.noticeEl = this.msgBox.querySelector(`#${type}`);
         this.messageEl = this.noticeEl.querySelector("h2");
     }
 
-    notify(msg, type) {
+    notify(msg) {
         this.noticeEl.classList.remove("notify-close");
         this.noticeEl.classList.add("notify-open");
-        if (type === "success" || type === "warning" || type === "error") {
-            this.noticeEl.classList.add(`${type}-msg`);
+        if (
+            this.msgType === "success" ||
+            this.msgType === "warning" ||
+            this.msgType === "error"
+        ) {
+            this.noticeEl.classList.add(`${this.msgType}-msg`);
         }
         this.messageEl.innerHTML = `${msg}`;
 
         setTimeout(() => {
             this.noticeEl.classList.remove("notify-open");
             this.noticeEl.classList.add("notify-close");
-            this.noticeEl.classList.remove(`${type}-msg`);
+            this.noticeEl.classList.remove(`${this.msgType}-msg`);
             this.messageEl.innerHTML = "";
         }, 5000);
     }
