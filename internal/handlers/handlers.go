d70421a62abc720606760e26fedc23cf65ba5264
Max Kranker <maxkranker@protonmail.com>
2026-01-19 16:09:50 +0700

    Add arrival_date, departure_date, and room_id json data

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -230,8 +230,11 @@ func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
 }
 
 type jsonResponse struct {
-	OK      bool   `json:"ok"`
-	Message string `json:"message"`
+	OK            bool   `json:"ok"`
+	Message       string `json:"message"`
+	RoomID        string `json:"room_id"`
+	ArrivalDate   string `json:"arrival_date"`
+	DepartureDate string `json:"departure_date"`
 }
 
 // AvailabilityJSON handles request for availability and sends JSON response
@@ -264,8 +267,11 @@ func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
 	}
 
 	resp := jsonResponse{
-		OK:      available,
-		Message: "",
+		OK:            available,
+		Message:       "",
+		ArrivalDate:   ad,
+		DepartureDate: dd,
+		RoomID:        strconv.Itoa(roomID),
 	}
 
 	out, err := json.MarshalIndent(resp, "", "     ")
