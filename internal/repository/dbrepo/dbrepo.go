06d147b74a6a740cf608ae0d93da326fd574de22
Max Kranker <maxkranker@protonmail.com>
2026-01-20 15:49:05 +0700

    Change to using *pgx.Conn

--- internal/repository/dbrepo/dbrepo.go
+++ internal/repository/dbrepo/dbrepo.go
@@ -1,18 +1,17 @@
 package dbrepo
 
 import (
-	"database/sql"
-
 	"github.com/AutomationMK/bookings/internal/config"
 	"github.com/AutomationMK/bookings/internal/repository"
+	"github.com/jackc/pgx/v5"
 )
 
 type postgresDBRepo struct {
 	App *config.AppConfig
-	DB  *sql.DB
+	DB  *pgx.Conn
 }
 
-func NewPostgresRepo(conn *sql.DB, a *config.AppConfig) repository.DatabaseRepo {
+func NewPostgresRepo(conn *pgx.Conn, a *config.AppConfig) repository.DatabaseRepo {
 	return &postgresDBRepo{
 		App: a,
 		DB:  conn,
