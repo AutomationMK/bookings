3c051be3f8c66514cca6bea4b02c8d8e2f1fd67a
Max Kranker <maxkranker@protonmail.com>
2026-01-23 21:50:02 +0700

    Add Auth middleware for checking if session has logged in user

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/AutomationMK/bookings/internal/helpers"
 	"github.com/justinas/nosurf"
 )
 
@@ -22,4 +23,19 @@ func NoSurf(next http.Handler) http.Handler {
 // SessionLoad loads and saves the session on every request
 func SessionLoad(next http.Handler) http.Handler {
 	return session.LoadAndSave(next)
+}
+
+// Auth checks if a session has a logged in user
+// if not logged in then user is directed to log in
+// and redirected to the /user/login route otherwise
+// the handler function is passed to use the next handler
+func Auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !helpers.IsAuthenticated(r) {
+			session.Put(r.Context(), "error", "You must be logged in first")
+			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
