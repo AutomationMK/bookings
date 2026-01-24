33fc306b82cedfe1864ec77ec3d6948e1610823a
Max Kranker <maxkranker@protonmail.com>
2026-01-24 13:07:14 +0700

    Add IsAuthenticated to TemplateData struct

--- internal/models/templatedata.go
+++ internal/models/templatedata.go
@@ -4,15 +4,16 @@ import "github.com/AutomationMK/bookings/internal/forms"
 
 // TemplateData holds data sent from handlers to templates
 type TemplateData struct {
-	StringMap map[string]string
-	IntMap    map[string]int
-	FloatMap  map[string]float32
-	Data      map[string]any
-	CSRFToken string
-	Flash     string
-	Warning   string
-	Error     string
-	Form      *forms.Form
+	StringMap       map[string]string
+	IntMap          map[string]int
+	FloatMap        map[string]float32
+	Data            map[string]any
+	CSRFToken       string
+	Flash           string
+	Warning         string
+	Error           string
+	Form            *forms.Form
+	IsAuthenticated int
 }
 
 // TemplateEmailData holds data sent from handlers to templates
