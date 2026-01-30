48860dbf2cea1769f2631f7a58749ee8ed2ddccf
Max Kranker <maxkranker@protonmail.com>
2026-01-30 16:42:45 +0700

    Fixed bug with missing comma for next row insert

--- migrations/20260121064525_seed_restritions_table.postgres.up.sql
+++ migrations/20260121064525_seed_restritions_table.postgres.up.sql
@@ -1,3 +1,3 @@
 INSERT INTO public.restrictions (restriction_name, created_at, updated_at) VALUES
-('Reservation', '2026-01-17 00:00:00.000', '2026-01-17 00:00:00.000')
+('Reservation', '2026-01-17 00:00:00.000', '2026-01-17 00:00:00.000'),
 ('Owner Block', '2026-01-17 00:00:00.000', '2026-01-17 00:00:00.000');
