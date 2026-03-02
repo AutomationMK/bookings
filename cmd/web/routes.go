6d2aeb2fa5c8918bc8dfa97b4eaced63b94a3db1
Max Kranker <maxkranker@protonmail.com>
2026-03-02 12:07:02 +0700

    Add /admin/delete-reservation/{src}/{id} route

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -43,6 +43,7 @@ func routes(app *config.AppConfig) http.Handler {
 		mux.Post("/dashboard/reservations/{src}/{id}", handlers.Repo.AdminPostShowReservation)
 
 		mux.Get("/process-reservation/{src}/{id}", handlers.Repo.AdminProcessReservation)
+		mux.Get("/delete-reservation/{src}/{id}", handlers.Repo.AdminDeleteReservation)
 	})
 
 	mux.Route("/fetch/admin", func(mux chi.Router) {
